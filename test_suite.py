0e85b657cdc000d9146dd55dbd1dd5c37619e8e9
glek <glek@berea.edu>
2024-04-21 14:46:55 -0400

    Began writing the test suites for our maincode.py file

--- test_suite.py
+++ test_suite.py
@@ -2,3 +2,15 @@ import unittest
 
 from maincode import Question, Quiz
 
+
+class TestQuiz(unittest.Testcase):
+
+
+
+    def setUp(self):
+    #This function will set up sample questions for testing
+        self.questions = [
+            Question("Test Question 1?", "Answer1", ["Answer1","Option 2", "Option 3", "Option 4"]),
+
+        ]
+

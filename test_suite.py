232f117dde38bf000f5b38a2c585612d77ec78bf
glek <glek@berea.edu>
2024-04-22 10:58:21 -0400

    Began writing the test suites for our maincode.py file

--- test_suite.py
+++ test_suite.py
@@ -4,13 +4,19 @@ from maincode import Question, Quiz
 
 
 class TestQuiz(unittest.Testcase):
-
-
-
     def setUp(self):
     #This function will set up sample questions for testing
         self.questions = [
             Question("Test Question 1?", "Answer1", ["Answer1","Option 2", "Option 3", "Option 4"]),
-
         ]
+
+    def test_questions_lading(self):
+        # Test if questions are loaded correctly
+        quiz = Quiz(self.questions)
+        self.assertEqual(len(quiz.questions), 2)
+
+    def test_timer (self):
+        # Test answer submission and scoring
+        quiz = Quiz(self.questions)
+
 
